e1d788ae6b0f17798d3a9266afaec9a3743ac4e5
lcym346-byte <lcym346@gmail.com>
2026-05-23 00:07:38 +0800

    Create firebase-config.js

---
+++ js/core/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase 連線設定
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-exRnhaxaq9poi9Hdwscz96GYeokA8KU",
+  authDomain: "stockflow-web-pro.firebaseapp.com",
+  databaseURL: "https://stockflow-web-pro-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "stockflow-web-pro",
+  storageBucket: "stockflow-web-pro.firebasestorage.app",
+  messagingSenderId: "464566024533",
+  appId: "1:464566024533:web:ef2264d4e0910c32be17dc"
+};
